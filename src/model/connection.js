ac78e44b168a31d7251d8ee7f5b838989ac446ed
Caio Felipe <imcaiofelipe@gmail.com>
2021-03-16 14:20:03 -0300

    last commit

--- src/model/connection.js
+++ src/model/connection.js
@@ -5,9 +5,26 @@ module.exports = mysql.createConnection({
     password: 'caio123',
     database: 'quizzy'
 }) */
-module.exports = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.AZURE_URL,
     user: process.env.AZURE_USER,
     password: process.env.AZURE_PASSWORD,
     database: process.env.AZURE_DB,
 })
+
+function connectDB() {
+    connection.connect(function(error){
+        if(!!error) console.log(error);
+         else console.log('SQL Database Connected!');
+    });
+}
+
+connectDB()
+
+connection.on('error', err => {
+    console.log(err)
+
+    setTimeout(connectDB, 1000)    
+})
+
+module.exports = connection
