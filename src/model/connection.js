32f6bf8ca03ab1f04051e2130640ee0c6b02b484
Caio <imcaiofelipe@gmail.com>
2021-08-26 12:46:45 -0300

    Configurações de deploy adicionadas.

--- src/model/connection.js
+++ src/model/connection.js
@@ -5,9 +5,27 @@ const mysql = require('mysql2')
     password: '*senha*',
     database: '*db_nome*'
 }) */
-module.exports = mysql.createPool({
+let connection = mysql.createConnection({
     user: process.env.AWS_USER,
     password: process.env.AWS_PASSWORD,
     database: process.env.AWS_DB,
     host: process.env.AWS_HOST
 })
+
+function connectDB() {
+    connection.connect(function(error){
+        if(!!error) console.log(error);
+         else console.log('SQL Database Connected!');
+    });
+}
+
+connectDB()
+
+connection.on('error', err => {
+    console.log(err)
+})
+connection.on('uncaughtException', err => {
+    console.log(err)
+})
+
+module.exports = connection
