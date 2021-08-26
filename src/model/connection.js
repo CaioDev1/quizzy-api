12584fdd8f2b07cd50d7c8380cd79cbfabe62eee
Caio <imcaiofelipe@gmail.com>
2021-08-26 13:14:49 -0300

    Configurações de deploy adicionadas.

--- src/model/connection.js
+++ src/model/connection.js
@@ -9,7 +9,8 @@ let connection = mysql.createConnection({
     user: process.env.AWS_USER,
     password: process.env.AWS_PASSWORD,
     database: process.env.AWS_DB,
-    host: process.env.AWS_HOST
+    host: process.env.AWS_HOST,
+    connectTimeout: 30
 })
 
 function connectDB() {
